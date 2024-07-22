51d43e99488f6911822d11271aad0994bcfb9eb1
Bruno Montezano <brunodoyt@gmail.com>
2024-07-22 15:17:50 -0300

    add analysis scripts with to-do tasks

---
+++ 01_analysis.r
@@ -0,0 +1,7 @@
+df <- readr::read_rds("./data/exported/pre_post.rds")
+
+# TODO: Tests for normality
+
+# TODO: Compare treatment arm vs. waiting list (one-tailed)
+
+# TODO: Descriptive tables (need to update `demog_table.xlsx`)
